d78f95b64fbbe17d1d587ba360b8844cdd89e77d
Alejandro Mendez <alejmendez.87@gmail.com>
2017-11-13 10:26:59 -0400

    cambio de estructura de escritorio

--- app/Modules/Base/Http/Controllers/EscritorioController.php
+++ app/Modules/Base/Http/Controllers/EscritorioController.php
@@ -5,62 +5,54 @@ namespace marygastro\Modules\Base\Http\Controllers;
 use marygastro\Modules\Base\Http\Controllers\Controller;
 
 class EscritorioController extends Controller {
-	public $autenticar = false;
+    public $autenticar = false;
 
-	protected $titulo = 'Escritorio';
-	protected $prefijo = 'backend';
-	public function __construct() {
-		parent::__construct();
+    protected $titulo = 'Escritorio';
+    protected $prefijo = 'backend';
 
-		$this->middleware('auth');
-	}
+    public function __construct() {
+        parent::__construct();
 
-	public function getIndex() {
+        $this->middleware('auth');
+    }
 
-			$data = '';
-			$user ='';
-		\Mail::send('pagina::emails.notificacion', $data, function ($message) use ($user){
+    public function getIndex() {
+        $data = '';
+        $user ='';
+        \Mail::send('pagina::emails.notificacion', $data, function ($message) use ($user){
+            $message->subject('Asunto del correo');
+            $message->to('drummermiguelangel@gmail.com');
+        });
+        
+        $permisos = [
+            'incidencias/inicio/usuarios',
+            'incidencias/escritorios/tecnicos',
+        ];
 
-			$message->subject('Asunto del correo');
-
-			$message->to('drummermiguelangel@gmail.com');
-
-		});
-		
-
-
-		$permisos = [
-			'incidencias/inicio/usuarios',
-			'incidencias/escritorios/tecnicos',
-			
-		];
-		$pase = 0;
-		$ultimoPermiso = '';
-		foreach ($permisos as $permiso) {
-			
-			if ($this->permisologia($permiso)) {
-				$pase++;
-				$ultimoPermiso = $permiso;
-			}
-		}
-		/* if(\Auth::user()->super == 's'){
-			
-			return $this->view('base::Escritorio');
-		} */
-		
-		if(\Auth::user()->perfil->nombre == "Usuarios") {
-			return redirect($this->prefijo . '/incidencias/inicio/usuarios');
-		}
-		if(\Auth::user()->perfil->nombre == "Tecnicos") {
-			return redirect($this->prefijo . '/incidencias/escritorios/tecnicos');
-		}
-		
-		if ($pase >= 1){
-			
-			return $this->view('base::Escritorio');
-		} else {
-			
-			return $this->view('base::Escritorio');
-		}
-	}
+        $pase = 0;
+        $ultimoPermiso = '';
+        foreach ($permisos as $permiso) {
+            if ($this->permisologia($permiso)) {
+                $pase++;
+                $ultimoPermiso = $permiso;
+            }
+        }
+        /* if(\Auth::user()->super == 's'){
+            
+            return $this->view('base::Escritorio');
+        } */
+        
+        if(\Auth::user()->perfil->nombre == "Usuarios") {
+            return redirect($this->prefijo . '/incidencias/inicio/usuarios');
+        }
+        if(\Auth::user()->perfil->nombre == "Tecnicos") {
+            return redirect($this->prefijo . '/incidencias/escritorios/tecnicos');
+        }
+        
+        if ($pase >= 1){
+            return $this->view('base::Escritorio');
+        } else {
+            return $this->view('base::Escritorio');
+        }
+    }
 }
