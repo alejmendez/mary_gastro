b8c5ef1395aafcf8dee004a109ae3036c10f9519
Alejandro Mendez <alejmendez.87@gmail.com>
2017-11-17 09:43:16 -0400

    cambios en escritorio

--- app/Modules/Base/Http/Controllers/EscritorioController.php
+++ app/Modules/Base/Http/Controllers/EscritorioController.php
@@ -5,55 +5,6 @@ namespace marygastro\Modules\Base\Http\Controllers;
 use marygastro\Modules\Base\Http\Controllers\Controller;
 
 class EscritorioController extends Controller {
-<<<<<<< HEAD
-	public $autenticar = false;
-
-	protected $titulo = 'Escritorio';
-	protected $prefijo = 'backend';
-	public function __construct() {
-		parent::__construct();
-
-		$this->middleware('auth');
-	}
-
-	public function getIndex() {
-
-
-		$permisos = [
-			'incidencias/inicio/usuarios',
-			'incidencias/escritorios/tecnicos',
-			
-		];
-		$pase = 0;
-		$ultimoPermiso = '';
-		foreach ($permisos as $permiso) {
-			
-			if ($this->permisologia($permiso)) {
-				$pase++;
-				$ultimoPermiso = $permiso;
-			}
-		}
-		/* if(\Auth::user()->super == 's'){
-			
-			return $this->view('base::Escritorio');
-		} */
-		
-		if(\Auth::user()->perfil->nombre == "Usuarios") {
-			return redirect($this->prefijo . '/incidencias/inicio/usuarios');
-		}
-		if(\Auth::user()->perfil->nombre == "Tecnicos") {
-			return redirect($this->prefijo . '/incidencias/escritorios/tecnicos');
-		}
-		
-		if ($pase >= 1){
-			
-			return $this->view('base::Escritorio');
-		} else {
-			
-			return $this->view('base::Escritorio');
-		}
-	}
-=======
     public $autenticar = false;
 
     protected $titulo = 'Escritorio';
@@ -104,5 +55,4 @@ class EscritorioController extends Controller {
             return $this->view('base::Escritorio');
         }
     }
->>>>>>> d78f95b64fbbe17d1d587ba360b8844cdd89e77d
 }
