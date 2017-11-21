13d7969c706f2be53141b1f551ca97949163320d
Maria Campora <camporamaria@gmail.com>
2017-11-21 11:57:54 -0400

    hoy

--- storage/framework/views/8e87172a6765a8e5bc1e683e0b827e97d7bbe141.php
+++ storage/framework/views/8e87172a6765a8e5bc1e683e0b827e97d7bbe141.php
@@ -5,7 +5,6 @@
 		<h3 class="styled-font">Consejos y Recomendaciones</h3>
 	</div>
 </section>
-
 <section class="page-info">
 	<div class="auto-container clearfix">
 		<div class="breadcrumb-outer">
@@ -21,54 +20,30 @@
 <div class="row clearfix">
 <div class="content-side  col-lg-9 col-md-8 col-sm-12 col-xs-12 ">
 	<section class="blog-classic-view">
-		<?php $__currentLoopData = $noticias; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $noticia): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-			<?php
-			$ncat = clone $ncategoria;
-			$nomcat = clone $nombrecat;
-			$ncat = $ncat->where('noticias_id', $noticia->id)->get();
-			foreach ($ncat as $_categoria){
-				$nomcat = $nomcat->where('id',$_categoria->categorias_id)->first();
-				$_ncat[] = $nomcat->nombre;
-			}
-			?>
-			<div class="news-style-one list-style">
+		<div id="post-323" class="post-323 post type-post status-publish format-standard has-post-thumbnail hentry category-our-blog tag-foods tag-workouts">
+			<div class="news-style-one">
 				<div class="inner-box">
-					<div class="row clearfix">
-						<div class="image-column col-lg-4 col-md-5 col-sm-4 col-xs-12">
-							<figure class="image-box">
-								<a href="<?php echo e(url('/blog/noticia/'. $noticia->slug)); ?>">
-									<img width="270" height="247" src="<?php echo e(url('public/archivos/noticias/'.$noticia->imagenes->first()->archivo)); ?>" class="attachment-270x247 size-270x247 wp-post-image" alt="blog-image-15">
-								</a>
-							</figure>
+					<figure class="image-box">
+						<a href="#">
+							<img width="870" height="500" src="<?php echo e(url('public/archivos/noticias/' . $noticia->imagenes->first()->archivo)); ?>" class="attachment-1170x500 size-1170x500 wp-post-image" alt="blog-image-11" />
+						</a>
+					</figure>
+					<div class="lower-content">
+						<div class="posted-info">
+							<?php echo e($controller->meses[$noticia->published_at->month]); ?>
+
+							<?php echo e($noticia->published_at->day); ?>,
+							<?php echo e($noticia->published_at->year); ?>
+
 						</div>
-						<div class="content-column col-lg-8 col-md-7 col-sm-8 col-xs-12">
-							<div class="lower-content">
-								<div class="posted-info">
-									<?php echo e($noticia->published_at->format('d/m/Y h:m a')); ?>
-
-								</div>
-								<h3>
-									<a href="<?php echo e(url('/blog/noticia/'. $noticia->slug)); ?>">
-										<?php echo e($noticia->titulo); ?>
-
-									</a>
-								</h3>
-								<div class="text">
-									<p><?php echo str_replace("\n", '<br/>', $controller->limit_text($noticia->resumen,60)); ?></p>
-								</div>
-								<div class="">
-									<?php $__currentLoopData = $_ncat; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $ncat): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-										<?php echo e($ncat); ?>
-
-									<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-								</div>
-							</div>
+						<h3><a href="#"><?php echo e($noticia->titulo); ?></a></h3>
+						<div class="text">
+							<p><?php echo $noticia->contenido_html; ?></p>
 						</div>
 					</div>
 				</div>
 			</div>
-		<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-		<center><?php echo e($noticias->render()); ?></center>
+		</div>
 	</section>
 </div>
 
@@ -90,7 +65,7 @@
 			</div>
 			<ul>
 				<?php $__currentLoopData = $categorias; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $categoria): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-					<li class="cat-item cat-item-24"><a href="<?php echo e(url('/blog/'.str_slug($categoria->nombre))); ?>" ><?php echo e($categoria->nombre); ?></a> (<?php echo e($categoria->total); ?>)</li>
+					<li class="cat-item cat-item-24"><a href="<?php echo e(route('pag.categoria', ['slug' => str_slug($categoria->nombre)])); ?>" ><?php echo e($categoria->nombre); ?></a> (<?php echo e($categoria->total); ?>)</li>
 				<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 			</ul>
 		</div>
@@ -99,15 +74,15 @@
 				<div class="sidebar-title">
 					<h3>Blogs Recientes</h3>
 				</div>
-				<?php $__currentLoopData = $noticias; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $noticia): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+				<?php $__currentLoopData = $listaNoticias; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $noticia): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
 					<div class="post">
 						<figure class="post-thumb">
-							<img width="75" height="75" src="<?php echo e(url('public/archivos/noticias/'.$noticia->imagenes->first())); ?>" class="attachment-75x75 size-75x75 wp-post-image" alt="5"  sizes="(max-width: 75px) 100vw, 75px" />
+							<img width="75" height="75" src="<?php echo e(url('public/archivos/noticias/' . $noticia->imagenes->first()->archivo)); ?>" class="attachment-75x75 size-75x75 wp-post-image" alt="5"  sizes="(max-width: 75px) 100vw, 75px" />
 							<a href="#" class="overlay-link">
 								<span class="fa fa-link"></span>
 							</a>
 						</figure>
-						<div class="desc-text"><a href="<?php echo e(url('/blog/noticia/'. $noticia->slug)); ?>"><?php echo e($noticia->titulo); ?> ...</a></div>
+						<div class="desc-text"><a href="<?php echo e(route('pag.blog', ['slug' => $noticia->slug])); ?>"><?php echo e($noticia->titulo); ?> ...</a></div>
 						<div class="time">
 							<?php echo e($controller->meses[$noticia->published_at->month]); ?>
 
@@ -122,5 +97,4 @@
 	</aside>
 </div>
 <?php $__env->stopSection(); ?>
-
 <?php echo $__env->make('pagina::layouts.default', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
