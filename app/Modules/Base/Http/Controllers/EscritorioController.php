89bfc6a9f6195897ff778be91d8942318140e2d8
Alejandro Mendez <alejmendez.87@gmail.com>
2017-11-23 09:44:40 -0400

    correccion de error en escritorio

--- app/Modules/Base/Http/Controllers/EscritorioController.php
+++ app/Modules/Base/Http/Controllers/EscritorioController.php
@@ -17,15 +17,7 @@ class EscritorioController extends Controller {
     }
 
     public function getIndex() {
-        $data = [
-            'mensaje' => 'Este es el cuerpo del msj'
-        ];
         $user = \Auth::user();
-        \Mail::send('pagina::emails.notificacion', $data, function ($message) use ($user){
-            $message->subject('Asunto del correo');
-            $message->to('alejmendez.87@gmail.com');
-            $message->to('drummermiguelangel@gmail.com');
-        });
         
         $permisos = [
             'incidencias/inicio/usuarios',
